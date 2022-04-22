52ace669b7840a46198697bae5f3fbd98c7f908f
Ramil Galikeev <galikeevramil95@gmail.com>
2022-04-22 16:10:30 +0500

    changed styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { BrowserRouter } from "react-router-dom";
+import AppRouter from "./components/AppRouter";
+import NavBar from "./components/NavBar";
 
 
 const App = () => {
 	return (
 		<div className="app">
-			Hello
+			<BrowserRouter>
+				<NavBar/>
+				<AppRouter/>
+			</BrowserRouter>
 		</div>
 	);
 }
